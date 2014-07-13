ca7bc26ac1eb2f1d819a6211de7617b6f55414c1
houxh <houxuehua49@gmail.com>
2014-07-14 00:21:25 +0800

    添加set.go

---
+++ set.go
@@ -0,0 +1,28 @@
+package main
+
+type IntSet map[int64]struct{}
+
+func NewIntSet() IntSet {
+    return make(map[int64]struct{})
+}
+
+func (set IntSet) Add(v int64) {
+    if _, ok := set[v]; ok {
+        return
+    }
+    set[v] = struct{}{}
+}
+
+func (set IntSet) IsMember(v int64) bool {
+    if _, ok := set[v]; ok {
+        return true
+    }
+    return false
+}
+
+func (set IntSet) Remove(v int64) {
+    if _, ok := set[v]; !ok {
+        return
+    }
+    delete(set, v)
+}
