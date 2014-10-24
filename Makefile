7352297621e573f360a977319268e9ceebfc7112
houxh <houxuehua49@gmail.com>
2014-10-24 10:31:16 +0800

    压力测试脚本

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-all:im benchmark benchmark_connection
+all:im benchmark benchmark_connection benchmark_sender
 
 im:im.go peer.go peer_client.go client.go cluster.go route.go protocol.go storage.go group_server.go group_manager.go group.go set.go state_center.go config.go monitoring.go
 	go build im.go peer.go peer_client.go client.go cluster.go route.go protocol.go storage.go group_server.go group_manager.go group.go set.go state_center.go config.go monitoring.go
@@ -9,10 +9,14 @@ benchmark:benchmark.go protocol.go
 benchmark_connection:benchmark_connection.go protocol.go
 	go build benchmark_connection.go protocol.go
 
+benchmark_sender:benchmark_sender.go protocol.go
+	go build benchmark_sender.go protocol.go
+
 install:all
 	cp im ./bin
 	cp benchmark ./bin
 	cp benchmark_connection ./bin
+	cp benchmark_sender ./bin
 	cp push.py ./bin
 clean:
 	rm -f im benchmark benchmark_connection
---
+++ benchmark_sender.go
@@ -0,0 +1,64 @@
+package main
+import "fmt"
+import "net"
+import "log"
+import "runtime"
+import "flag"
+import "math/rand"
+
+var first int64
+var last int64
+var host string
+var port int
+
+func init() {
+    flag.Int64Var(&first, "first", 0, "first uid")
+    flag.Int64Var(&last, "last", 0, "last uid")
+
+    flag.StringVar(&host, "host", "127.0.0.1", "host")
+    flag.IntVar(&port, "port", 23000, "port")
+}
+
+func send(uid int64) {
+    ip := net.ParseIP(host)
+    addr := net.TCPAddr{ip, port, ""}
+
+    conn, err := net.DialTCP("tcp4", nil, &addr)
+    if err != nil {
+        log.Println("connect error")
+        return
+    }
+    seq := 1
+    
+    SendMessage(conn, &Message{MSG_AUTH, seq, &Authentication{uid:uid, platform_id:PLATFORM_IOS}})
+    ReceiveMessage(conn)
+
+    for i := 0; i < 18000; i++ {
+        r := rand.Int63()
+        receiver := r%(last-first)  + first
+        log.Println("receiver:", receiver)
+        content := fmt.Sprintf("test....%d", i)
+        seq++
+        msg := &Message{MSG_IM, seq, &IMMessage{uid, receiver, 0, int32(i), content}}
+        SendMessage(conn, msg)
+        for {
+            ack := ReceiveMessage(conn)
+            if ack.cmd == MSG_ACK {
+                break
+            }
+        }
+    }
+    conn.Close()
+    log.Printf("%d send complete", uid)
+}
+
+func main() {
+    runtime.GOMAXPROCS(4)
+    flag.Parse()
+    fmt.Printf("first:%d, last:%d\n", first, last)
+    if last <= first {
+        return
+    }
+	log.SetFlags(log.Lshortfile|log.LstdFlags)
+    send(1)
+}
