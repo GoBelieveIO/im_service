bd81c80ad25d1ac2796107fc7bb3925140ed2299
houxh <houxuehua49@gmail.com>
2015-12-10 17:08:31 +0800

    将消息长度限制为32K

--- protocol.go
+++ protocol.go
@@ -928,7 +928,7 @@ func ReceiveMessage(conn io.Reader) *Message {
 	}
 
 	length, seq, cmd, version := ReadHeader(buff)
-	if length < 0 || length >= 4*1024*1024 {
+	if length < 0 || length >= 32*1024 {
 		log.Info("invalid len:", length)
 		return nil
 	}
