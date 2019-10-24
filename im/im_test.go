842865b09de7e0816a38677fef2d6032c04cf005
houxh <houxuehua49@gmail.com>
2019-10-24 16:22:55 +0800

    群组管理事件通过redis的stream传递

--- im/im_test.go
+++ im/im_test.go
@@ -16,10 +16,19 @@ var relationship_pool *RelationshipPool
 
 func init() {
 	filter = sensitive.New()
-	relationship_pool = NewRelationshipPool()	
 }
 
-
+type GroupEvent struct {
+	Id string //stream entry id
+	ActionId int64 `redis:"action_id"`
+	PreviousActionId int64 `redis:"previous_action_id"`
+	Name string `redis:"name"`
+	AppId int64 `redis:"app_id"`
+	GroupId int64 `redis:"group_id"`
+	MemberId int64 `redis:"member_id"`
+	IsSuper bool `redis:"super"`
+	IsMute bool `redis:"mute"`
+}
 
 
 func NewRedisPool(server, password string, db int) *redis.Pool {
@@ -58,9 +67,10 @@ func TestMain(m *testing.M) {
 		log.Println("usage:main.test config_file")
 		return
 	}
-	
-	config = read_cfg(flag.Args()[0])
 
+	config = read_cfg(flag.Args()[0])
+	
+	relationship_pool = NewRelationshipPool()
 	redis_pool = NewRedisPool(config.redis_address, config.redis_password, 
 		config.redis_db)	
 
@@ -101,7 +111,7 @@ func FilterDirtyWord(msg *IMMessage) {
 }
 
 
-func Test_Filter(t *testing.T) {
+func TestFilter(t *testing.T) {
 	log.Println("hhhhh test")
 
 	msg := &IMMessage{}
@@ -128,6 +138,8 @@ func Test_Relationship(t *testing.T) {
 	relationship_pool.SetInYourBlacklist(7, 1, 2, true)
 
 	rs = relationship_pool.GetRelationship(7, 1, 2)
+
+	log.Println("rs:", rs, rs.IsMyFriend(), rs.IsYourFriend(), rs.IsInMyBlacklist(), rs.IsInYourBlacklist())
 	
 	if !rs.IsMyFriend() || !rs.IsYourFriend() || !rs.IsInMyBlacklist() || !rs.IsInYourBlacklist() {
 		t.Error("error")
@@ -155,7 +167,99 @@ func Test_Relationship(t *testing.T) {
 	
 	log.Println("rs:", rs, rs.IsMyFriend(), rs.IsYourFriend(), rs.IsInMyBlacklist(), rs.IsInYourBlacklist())
 
-	
-
 }
 
+
+func TestStreamRange(t *testing.T) {
+	conn := redis_pool.Get()
+	defer conn.Close()
+
+	r, err := redis.Values(conn.Do("XREVRANGE", "test_stream", "+", "-", "COUNT", "1"))
+
+	if err != nil {
+		log.Println("redis err:", err)
+		return
+	}
+
+
+	for len(r) > 0 {
+		var entries []interface{}
+		r, err = redis.Scan(r, &entries)
+		if err != nil {
+			t.Error("redis err:", err)
+			return
+		}
+
+		var id string
+		var fields []interface{}
+		_, err = redis.Scan(entries, &id, &fields)
+		if err != nil {
+			t.Error("redis err:", err)
+			return			
+		}
+		log.Println("id:", id)
+
+
+		event := &GroupEvent{}
+		event.Id = id
+		err = redis.ScanStruct(fields, event)
+		if err != nil {
+			log.Println("scan err:", err)
+		}
+		log.Println("event:", event.Id, event.Name, event.GroupId, event.MemberId, event.IsSuper)
+	}
+}
+
+func TestStreamRead(t *testing.T) {
+	conn := redis_pool.Get()
+	defer conn.Close()
+
+	reply, err := redis.Values(conn.Do("XREAD", "COUNT", "2", "STREAMS", "test_stream", "0-0"))
+
+	if err != nil {
+		log.Println("redis err:", err)
+		return
+	}
+
+	var stream_res []interface{}
+	_, err = redis.Scan(reply, &stream_res)
+	if err != nil {
+		log.Println("redis scan err:", err)
+		return
+	}
+
+	var ss string	
+	var r []interface{}
+	_, err = redis.Scan(stream_res, &ss, &r)
+	if err != nil {
+		log.Println("redis scan err:", err)
+		return
+	}
+	
+	for len(r) > 0 {
+		var entries []interface{}
+		r, err = redis.Scan(r, &entries)
+		if err != nil {
+			t.Error("redis err:", err)
+			return
+		}
+
+		var id string
+		var fields []interface{}
+		_, err = redis.Scan(entries, &id, &fields)
+		if err != nil {
+			t.Error("redis err:", err)
+			return			
+		}
+		log.Println("id:", id)
+
+
+		event := &GroupEvent{}
+		event.Id = id
+		err = redis.ScanStruct(fields, event)
+		if err != nil {
+			log.Println("scan err:", err)
+		}
+		log.Println("event:", event.Id, event.Name, event.GroupId, event.MemberId, event.IsSuper)
+	}
+}
